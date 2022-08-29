2c2f2aa0b2f61aec64ac8fad867e7bd12fa968dd
Daniel Barbeira Hayes <daniel.barbeira@udc.es>
2022-08-30 00:09:48 +0200

    Initial project with DockerFile
    Basic project with a SpringBoot REST Controller with a basic request.
    Includes base Dockerfile for executing in a Docker container.

---
+++ src/test/java/com/amplia/microstreaminganalytics/MicroStreamingAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amplia.microstreaminganalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroStreamingAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
